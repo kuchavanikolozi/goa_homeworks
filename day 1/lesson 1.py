6c2e1dbab9b87f42d902e9016e18118f763c3eda
kuchavanikolozi <booking.rzakabot@gmail.com>
2024-02-29 19:19:31 +0400

    first commit

---
+++ day 1/lesson 1.py
@@ -0,0 +1,11 @@
+name = "Nika"
+surname = "Kuchava"
+age= 15
+height = 148.5
+knows_programming = False
+is_ugly = False
+
+print ("my name is" + " " +name + " "+ surname + " i am " + str (age)+" Years old " + " and i am " + str(height)+ " in height" )
+
+
+
---
+++ day 2/lesson 2.py
@@ -0,0 +1,8 @@
+#Who did I become friends with and who did I meet?
+friend = "Dati Chxikvadze"
+met = "Nika Kuchashvili, Mate Qevxishvili, Gio Gagnidze, and more... "
+
+#what i am think about GOA?
+think = "GOA is a good academy where you can transformate from Virgin to Chad, I'll promise GOA will help me reach goals"
+
+print(" I became friends with" + " " + friend + " I am also met" + " "  + met + " Im think," + " " + think )
