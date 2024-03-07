58f752f96916a4079f152615262be044dbc57e13
qweqwe1qq <qweqwe@gmail.com>
2024-03-07 18:30:30 +0400

    Revert "Delete day 4 directory"
    This reverts commit 531823fe4b6b448d4d9879a0addda80a96d4b6bb.

---
+++ day 4/lesson 4.py
@@ -0,0 +1,5 @@
+#we need to create a program, where consumer can input 2 numbers, example: 5+5 = 10
+
+first_number= int(input("Please input an number:"))
+second_number = int(input("Please input an another number:"))
+print(first_number+second_number)
