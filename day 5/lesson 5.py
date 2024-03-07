d0307ceb7b31b545c2b61f65ea31a1baf3d4ac05
qweqwe1qq <qweqwe@gmail.com>
2024-03-07 18:49:37 +0400

    second commit

--- day 5/lesson 5.py
+++ day 5/lesson 5.py
@@ -1,1 +1,23 @@
-#
+# We need to input two pointers to the user via the input() function, store them in variables, and then check what types of values are stored in the given variables using the type function
+# and we need to do 10 examples of division (/,//,%)
+
+# a = input()
+# b = input()
+
+# print(type(a))
+# print(type(b))
+
+print(12/7)
+print(34//11)
+print(12 % 5)
+
+print(88/5)
+print(56//8)
+print(44 % 3)
+
+print(67/9)
+print(46//3)
+print(22 % 5)
+
+print(98/15)
+
