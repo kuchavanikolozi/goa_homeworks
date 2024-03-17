d339d2ce16f604caed9e2076c67da50999e7fade
qweqwe1qq <qweqwe@gmail.com>
2024-03-17 10:44:43 +0400

    day 6

---
+++ day 6/lesson 6.py
@@ -0,0 +1,100 @@
+# We need to input an integer to the user and store it in a variable.
+# Let's also take a new variable and store the value of the first variable as a decimal.
+# Finally, let's compare these two numbers with the == operator and output them to the terminal.
+
+# number = int(input("Please input an integer number: "))
+# number2 = float(number)
+
+# print(number == number2)
+
+# We need to compile 5 instances of the > operator
+
+# print(5 > 5)
+# print(8 > 3)
+# print(12 > 21)
+# print(23 > 6)
+# print(54 > 98)
+
+# We need to compile 5 instances of the < operator
+
+# print(65 < 23)
+# print(34 < 78)
+# print(98 < 41)
+# print(23 < 21)
+# print(43 < 70)
+
+# We need to compile 5 instances of the >= operator
+
+# print(23 >= 12)
+# print(56 >= 34)
+# print(66 >= 66)
+# print(28 >= 33)
+# print(44 >= 12)
+
+# We need to compile 5 instances of the <= operator
+
+# print(77 <= 13)
+# print(43 <= 90)
+# print(23 <= 56)
+# print(31 <= 4)
+# print(123 <= 234)
+
+# We need to compile 5 instances of the == operator
+
+# print(89 == 123)
+# print(76 == 45)
+# print(287 == 140)
+# print(331 == 212)
+# print(990 == 443)
+
+
+# We need to compile 5 instances of the != operator
+
+# print(67 != 23)
+# print(34 != 42)
+# print(23 != 85)
+# print(76 != 34)
+# print(99 != 21)
+
+#We need to provide the user with two numbers and compare them with all the learned operators.
+
+# number3 = int(input("Please input an number: "))
+# number4 = int(input("Please input an another number: "))
+
+# print(number3 > number4)
+# print(number3 < number4)
+# print(number3 >= number4)
+# print(number3 <= number4)
+# print(number3 == number4)
+# print(number3 != number4)
+
+#We need to provide the user with number and compare that number with all the learned operators.
+
+# number5 = int(input("Please input an number: "))
+
+# print(number5 > number5)
+# print(number5 < number5)
+# print(number5 >= number5)
+# print(number5 <= number5)
+# print(number5 == number5)
+# print(number5 != number5)
+
+#We need to introduce a number to the user and compare it to a number 10 greater than it using all the learned operators.
+
+# number6 = int(input("Please input an number: "))
+
+# print(number6 > 20)
+# print(number6 < 20)
+# print(number6 >= 20)
+# print(number6 <= 20)
+# print(number6 == 20)
+# print(number6 != 20)
+
+#We need to enter the user an integer in the first input, and a decimal in the second input, Finally, let's check the data types of the variable values with the == and != operators.
+
+# number7 = int(input("Please input an integer number: "))
+# number8 = float(input("Please input an float number: "))
+
+# print(number7 == number8)
+# print(number7 != number8)
+
