79661af538dba6724f661f43652d6e180f900cac
kuchavanikolozi <booking.rzakabot@gmail.com>
2024-03-24 15:05:59 +0400

    day 7

---
+++ day 7/lesson 7.py
@@ -0,0 +1,20 @@
+#We need to create an algorithm that will ask the user for the temperature in Celsius and convert it to Fahrenheit. ((variable * 1.8) + 32)
+
+# celsius = float(input("Please input an celsius: "))
+# fahrenheit = (celsius * 1.8) + 32
+
+# print(fahrenheit)
+
+# We need to ask the user for age and make the comparison so that age is less than twenty and greater than twelve.
+
+# age = input("Please input your age: ")
+# print(20 < age and 12 > age)
+
+# We need the user to input the sides of the rectangle and print its area.
+# lenght = float(input("Please input an lenght of rectangle: "))
+# width = float(input("Please input an width: "))
+# print(lenght * width)
+
+# We need the user to enter a number and print if it is greater than 100 and a multiple of 9 using the And operator
+# number = int(input("Please input an number: "))
+# print(number > 100 and number % 9 == 0)
